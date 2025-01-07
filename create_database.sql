153185e3e6be4cdd44e1d1920726e6d073370964
Shanay Murdock <murdock.shanay@gmail.com>
2025-01-07 17:53:53 -0500

    Add database creation

---
+++ create_database.sql
@@ -0,0 +1,14 @@
+-- Build Database: D597_Task_1
+DROP DATABASE IF EXISTS "D597_Task_1";
+
+-- Create Database: "D597_Task_1"
+-- (WGU Webinar 2, 2024)
+CREATE DATABASE "D597_Task_1"
+    WITH
+    OWNER = postgres
+    ENCODING = "UTF8"
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
